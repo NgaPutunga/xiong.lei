c021f1649bfb463dc5d127cf680ade2712481b79
seanleipsu <seanleipsu@gmail.com>
2018-02-07 21:17:09 -0500

    Add files via upload

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
 # Use this shell script to compile (if necessary) your code and then execute it. Below is an example of what might be found in this file if your program was written in Python
-#
+# python3 in my computer to call python3.6, if the command is different for your computer, please feel free to change it.
 python3 ./src/donation-analytics.py ./input/itcont.txt ./input/percentile.txt ./output/repeat_donors.txt
 
